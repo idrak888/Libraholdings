463d35889338eac22fa47db7055650e56ef41add
idrak888 <playbox8g@gmail.com>
2020-05-28 14:59:55 +0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Main from '../layouts/Main';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <Main>
+                <div className="HomePage">
+                    <h1>Libraholdings</h1>
+                </div>
+            </Main>
+        )
+    }
+}
+
+export default HomePage;
